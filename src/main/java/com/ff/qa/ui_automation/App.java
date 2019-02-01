ea7a5f9a401056eac42fbab535f557ab9d236cff
veereshtest <veereshpalacharla@gmail.com>
2019-02-01 17:07:44 +0000

    test

---
+++ src/main/java/com/ff/qa/ui_automation/App.java
@@ -0,0 +1,13 @@
+package com.ff.qa.ui_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
